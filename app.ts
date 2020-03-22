0c9e198b39dd6ee580776b4f0021713b22076196
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2020-03-22 22:41:12 +0100

    [FIX] Functionnal type orm and typescript db c onnect

--- app.ts
+++ app.ts
@@ -7,35 +7,22 @@ const express = require('express')
 const app = express()
 
 // Needed packages
-const fs = require('fs');
-const xml2js = require('xml2js');
+// const fs = require('fs');
+// const xml2js = require('xml2js');
 
 // Twiice utilities
 const logger = require('twiice/debug');
 const db = require('twiice/db');
-const models = require('twiice/models')
-const views = require('twiice/views')
+// const models = require('twiice/models/models');
+// const views = require('twiice/views')
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
     
 })
 
-app.listen(3000, function () {
-    logger.info('Connecting database ...')
-    db.conn.authenticate()
-    .then(() => {
-        logger.info('Connection has been established successfully.');
-        logger.info('Loading models : Database structure')
-        ///////////
-        /// NOTE : Load all modules here, before syncing
-        ///////////
-        db.conn.sync({ force: true }).then(() => {
-            logger.info('Models loaded');
-            views.Views.__init__();
-        });
+db.connect.then(function () {
+    app.listen(3000, function() {
+        logger.info('Server up : listening on port 3000');
     })
-    .catch(err => {
-        logger.error('Unable to connect to the database:', err);
-    });
-})
+});
--- twiice/db.ts
+++ twiice/db.ts
@@ -1,24 +1,19 @@
 // In this file, we should handle database connection (SQL in case)
 
-// Config file which store every needed data
-const Config = require('config');
-const dbSettings = Config.postgresql
+export {};
 
-// Option 1: Passing parameters separately
-const sequelize = new Sequelize(dbSettings.database, dbSettings.user, dbSettings.password, {
-    host: dbSettings.host,
-    dialect: 'postgres',
-    logging: msg => logger.info(msg),
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+const logger = require('twiice/debug');
+
+// IMPORTS
+import {createConnection} from "typeorm";
+
+const connect = new Promise(function(resolve, reject) {
+    logger.info('Connecting database ...')
+    createConnection().then(async connection => {
+        resolve(logger.info('Database connection up ! Keep going !'));
+    }).catch(error => reject(logger.error("TypeORM connection error: ", error)));
 });
 
 module.exports = {
-    DB: Sequelize,
-    conn: sequelize,
-    model: Sequelize.Model
+    connect: connect,
 }
